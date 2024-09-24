b0f3b19a3d6458ce432f72c1e8d158927486149b
Nguyen Tien Minh <nguyentienminhntm07102004@gmail.com>
2024-09-24 09:47:23 +0700

    Cấu hình file env

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const routes = require("./routes/client/index.route.js");
+// Cấu hình thư viện cho dotenv
+require("dotenv").config();
 
 //Cấu hình port và app express
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 // Cấu hình template(pug)
 app.set("views", "./views");
